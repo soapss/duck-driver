8f5d362f770c111b5981f7840e1135d405342f32
soapss <76599851+soapss@users.noreply.github.com>
2023-09-14 01:21:55 -0400

    added logo

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,21 @@
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
+import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native'
 import React from 'react'
 import tw from 'twrnc'
 
 const HomeScreen = () => {
     return (
         <SafeAreaView style={tw `bg-white h-full`}>
-            <View>
-                
+            <View style={tw`p-5`}>
+                <Image 
+                style={{
+                    width: 100,
+                    height: 100,
+                    resizeMode: "contain",
+                }}
+                source={{
+                    uri: "https://links.papareact.com/gzs"
+                }}
+                />
             </View>
         </SafeAreaView>
     )
